dba09b39746f34e249ec345a52d1529c9b88cf20
mathieu <mathieu.perrin@insa-lyon.fr>
2022-11-20 12:50:35 +0200

    Removed date formatting

--- pages/inscriptions_list.php
+++ pages/inscriptions_list.php
@@ -8,8 +8,8 @@ $deplacements = get_deplacements();
 
 setlocale(LC_TIME, "fr_FR");
 
-$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
-$formatter2 = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+//$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+//$formatter2 = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
 
 $user_id = $_SESSION["user_id"];
 ?>
@@ -31,9 +31,9 @@ $user_id = $_SESSION["user_id"];
                 <div class="col-md-1"><img src="/assets/icon/cross-32x32.png" /></div>
             <?php endif ?>
             <div class="col-md-3 row-title"><b><?= $deplacement['nom'] ?></b></div>
-            <div class=col-md-3><?= $formatter->format(strtotime($deplacement['depart'])) ?></div>
-            <div class=col-md-3><?= $formatter->format(strtotime($deplacement['arrivee'])) ?></div>
-            <div class=col-md-2><?= $formatter2->format(strtotime($deplacement['limite'])) ?></div>
+            <div class=col-md-3><?= $deplacement['depart'] ?></div>
+            <div class=col-md-3><?= $deplacement['arrivee'] ?></div>
+            <div class=col-md-2><?= $deplacement['limite'] ?></div>
         </div>
     <?php endforeach ?>
 </main>
