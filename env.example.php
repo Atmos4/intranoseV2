bfe4a556347c6976575a9ec39be6f8acb6a3bae8
Atmos4 <artrist.perrin@gmail.com>
2022-12-31 18:36:45 +0100

    Add env file example

---
+++ env.example.php
@@ -0,0 +1,12 @@
+<?php
+/** Example environment file. 
+ * To have a valid configuration:
+ * - create a file named env.php
+ * - copy the content of this file
+ * - change the variables according to your environement */
+
+$db_host = "localhost"; // SQL Database host
+$db_name = "database_name"; // SQL Database name
+$db_user = "root"; // SQL Database username
+$db_password = ""; // SQL Database password
+$debug_mode = false; // Keep this false for production
