fea9069989490df355fb459804f800b9abe1d94e
Atmos4 <artrist.perrin@gmail.com>
2023-03-25 16:31:35 +0100

    38: improve password reset

--- pages/settings/settings_password_change.php
+++ pages/settings/settings_password_change.php
@@ -7,10 +7,8 @@ $user = em()->find(User::class, $user_id);
 $v = new Validator();
 $current_pass = $v->password("current_password")->label("Mot de passe actuel")->required()->autocomplete("current-password");
 $new_pass = $v->password("new_password")->autocomplete("new-password")->label("Nouveau mot de passe")->required()->secure();
-$confirm_pass = $v->password("confirm_password")->autocomplete("new-passord")->label("Confirmation")->required();
 
 $current_pass->condition(password_verify($current_pass->value, $user->password), "Mauvais mot de passe");
-$confirm_pass->condition($new_pass->value == $confirm_pass->value, "Les deux mots de passes ne correspondent pas");
 
 if (!empty($_POST) and $v->valid()) {
     $user->set_password($new_pass->value);
@@ -24,14 +22,9 @@ page("Changement de mot de passe");
 ?>
 <a href="/mon-profil#mon-compte" class="secondary"><i class="fas fa-caret-left"></i> Retour</a>
 <form method="post">
-
+    <input type="hidden" autocomplete="username" name="username" value="<?= $user->login ?>">
     <?= $v->render_validation() ?>
-
     <?= $current_pass->render() ?>
-
     <?= $new_pass->render() ?>
-
-    <?= $confirm_pass->render() ?>
-
     <input type="submit" name="submitPassword" value="Mettre à jour">
 </form>
