85798d4f425bf7f88f01f79e85ca924bdfce7f53
Atmos4 <artrist.perrin@gmail.com>
2023-04-03 18:45:35 +0200

    11: add categories migration

--- database/migrations/Version20230327154141.php
+++ database/migrations/Version20230327154141.php
@@ -14,13 +14,13 @@ final class Version20230327154141 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add categories';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, race_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_3AF346686E59D40D (race_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, race_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, removed TINYINT(1) NOT NULL, INDEX IDX_3AF346686E59D40D (race_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_3AF346686E59D40D FOREIGN KEY (race_id) REFERENCES races (id)');
         $this->addSql('ALTER TABLE race_entries ADD category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE race_entries ADD CONSTRAINT FK_324866B512469DE2 FOREIGN KEY (category_id) REFERENCES categories (id)');
