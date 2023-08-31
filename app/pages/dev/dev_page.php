741609cb35782f13c5dacf8e645b2c059cc5cf9b
Atmos4 <artrist.perrin@gmail.com>
2023-08-31 08:58:18 +0200

    73: add ovh in dev tools

--- app/pages/dev/dev_page.php
+++ app/pages/dev/dev_page.php
@@ -6,5 +6,6 @@ page("Dev") ?>
     <ul>
         <li><a href="/dev/create-user" role="button" class="contrast outline">Créer utilisateur</a></li>
         <li><a href="/dev/send-email" role="button" class="contrast outline">Test email</a></li>
+        <li><a href="/dev/test-ovh" role="button" class="contrast outline">OVH test</a></li>
     </ul>
 </nav>
