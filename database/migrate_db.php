16a8eea0e9fc9b91c67d5cd4556762c1297f0f1e
Atmos4 <artrist.perrin@gmail.com>
2023-10-01 18:26:54 +0200

    90: github actions, clean migrations

---
+++ database/migrate_db.php
@@ -0,0 +1,55 @@
+<?php
+restrict_dev();
+
+function map_permission(string $p): Permission
+{
+    switch ($p) {
+        case 'STAFF':
+            return Permission::STAFF;
+        case 'COACHSTAFF':
+            return Permission::COACHSTAFF;
+        case 'COACH':
+            return Permission::COACH;
+        case 'ROOT':
+            return Permission::ROOT;
+        case 'GUEST':
+            return Permission::GUEST;
+        case 'USER':
+        default:
+            return Permission::USER;
+    }
+}
+
+function map_gender(string $g): Gender
+{
+    if ($g == 'H') {
+        return Gender::M;
+    }
+    return Gender::W;
+}
+
+$database = new PDO(
+    "mysql:dbname=" . env("OLD_DB_NAME") . ";host=" . env("OLD_DB_HOST") . ";charset=utf8mb4",
+    env("OLD_DB_USER"),
+    env("OLD_DB_PASSWORD"),
+);
+$users = $database->query('SELECT * FROM licencies WHERE valid=1 and invisible=0')->fetchAll();
+foreach ($users as $user) {
+    $newUser = new User();
+    $newUser->last_name = $user['nom'];
+    $newUser->first_name = $user['prenom'];
+    $newUser->login = $user['login'];
+    $newUser->password = password_hash($user["prenom"], PASSWORD_DEFAULT);
+    $newUser->nose_email = $user['email'];
+    $newUser->real_email = $user['realmail'];
+    $newUser->phone = $user['telport'] ?? $user['tel'];
+    $newUser->permission = map_permission($user['perm']);
+    $newUser->gender = map_gender($user['sexe']);
+    $newUser->birthdate = date_create($user['ddn']);
+    em()->persist($newUser);
+}
+
+em()->flush();
+$count = count($users);
+
+echo "Inserted $count users";
--- engine/core/DB.php
+++ engine/core/DB.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use \Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 
 class DB extends Singleton
 {
@@ -14,6 +15,13 @@ class DB extends Singleton
 
     protected function __construct()
     {
+        $evm = new \Doctrine\Common\EventManager;
+
+        // Table Prefix
+        $tablePrefix = new \TablePrefix('orm_');
+        $evm->addEventListener(\Doctrine\ORM\Events::loadClassMetadata, $tablePrefix);
+
+
         $config = ORMSetup::createAttributeMetadataConfiguration(paths: array("database/models"), isDevMode: true);
         $connection = DriverManager::getConnection([
             'driver' => 'pdo_mysql',
@@ -21,11 +29,11 @@ class DB extends Singleton
             'password' => env("DB_PASSWORD"),
             'dbname' => env("DB_NAME"),
             'host' => env("DB_HOST"),
-            'charset' => 'utf8mb4'
+            'charset' => 'utf8mb4',
         ], $config);
         $config->addCustomDatetimeFunction('MONTH', Month::class);
         $config->addCustomDatetimeFunction('DAY', Day::class);
-        $this->entityManager = new EntityManager($connection, $config);
+        $this->entityManager = new EntityManager($connection, $config, $evm);
     }
 
     static function get()
@@ -76,4 +84,33 @@ class Day extends FunctionNode
 
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
+}
+
+class TablePrefix
+{
+    protected $prefix = '';
+
+    public function __construct($prefix)
+    {
+        $this->prefix = (string) $prefix;
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+
+        if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName() === $classMetadata->rootEntityName) {
+            $classMetadata->setPrimaryTable([
+                'name' => $this->prefix . $classMetadata->getTableName()
+            ]);
+        }
+
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadata::MANY_TO_MANY && $mapping['isOwningSide']) {
+                $mappedTableName = $mapping['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+
 }
