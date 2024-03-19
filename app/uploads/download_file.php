c90fc571075fe19efb761c4ec70754b97dab6404
Mathieu Perrin <mathieu.perrin@insa-lyon.fr>
2024-03-19 08:59:11 +0100

    173: fix download files

--- app/uploads/download_file.php
+++ app/uploads/download_file.php
@@ -3,11 +3,12 @@ restrict_access(Access::$ADD_EVENTS);
 $file_id = $_GET['id'];
 
 $file = em()->find(SharedFile::class, $file_id);
-$path = app_path() . "\/uploads/" . $file->path;
+$path = app_path() . "/uploads/" . $file->path;
 
 if (file_exists($path)) {
     header("Content-Type: " . $file->mime);
-    header("Content-Disposition: attachment; filename=" . $file->name);
+    header('Content-Disposition: attachment; filename="' . $file->name . '"');
     header("Content-Length: " . filesize($path));
     readfile($path);
+    exit;
 }
