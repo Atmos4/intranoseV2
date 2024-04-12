b11ed2df565285e54916e211d5541726094e0860
Mathieu Perrin <46241768+Mathieu414@users.noreply.github.com>
2024-04-12 09:41:24 +0200

    Acitivty delete (#216)
    * 210: activity delete, activity view update
    
    * 210: remove return in force_404

--- app/pages/events/activity_delete.php
+++ app/pages/events/activity_delete.php
@@ -1,39 +1,33 @@
 <?php
 restrict_access(Access::$ADD_EVENTS);
 
+$activity_id = get_route_param("activity_id");
 $event_id = get_route_param("event_id");
-$event = em()->find(Event::class, $event_id);
-if (!$event) {
-    force_404("the event of id $event_id doesn't exist");
-}
-if ($event->open) {
-    force_404("Cannot delete published event");
+$activity = em()->find(Activity::class, $activity_id);
+if (!$activity) {
+    force_404("the activity of id $activity_id doesn't exist");
 }
 if (!empty($_POST) and isset($_POST['delete'])) {
-    em()->remove($event);
+    em()->remove($activity);
     em()->flush();
-    redirect("/evenements");
+    redirect("/evenements/$event_id");
 }
 
 page("Confirmation de suppression");
 ?>
 <form method="post">
     <div class="row center">
-        <p>Sûr de vouloir supprimer cet événement?</p>
+        <p>Sûr de vouloir supprimer cette activité?</p>
         <p class="row">
             <span>
                 <i class="fa fa-chevron-right"></i>
-                Courses:
-                <?= count($event->activities) ?>
-            </span>
-            <span>
-                <i class="fa fa-chevron-right"></i>
                 Inscriptions:
-                <?= count($event->entries) ?>
+                <?= count($activity->entries) ?>
             </span>
         </p>
         <div class="col-auto">
-            <a class="secondary" role="button" href="/evenements/<?= $event_id ?>">Annuler</a>
+            <a class="secondary" role="button"
+                href="/evenements/<?= $event_id ?>/activite/<?= $activity_id ?>">Annuler</a>
         </div>
         <div class="col-auto">
             <button type="submit" name="delete" value="true" class="destructive">Supprimer</button>
