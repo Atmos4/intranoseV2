63036d2580d89a9ec63b0bd40b65914983c4fd1f
Atmos4 <atmos4.dev@gmail.com>
2024-11-10 23:09:30 +0100

    301: change icon size

--- app/components/finish_icon.php
+++ app/components/finish_icon.php
@@ -1,4 +1,4 @@
-<svg viewBox="0 0 100 100" width="30px" height="30px" stroke="currentColor" style="stroke-width: 10px;">
+<svg viewBox="0 0 100 100" width="25px" height="25px" stroke="currentColor" style="stroke-width: 10px;">
     <circle cx="50" cy="50" r="40" fill="none" />
     <circle cx="50" cy="50" r="20" fill="none" />
 </svg>
--- app/components/start_icon.php
+++ app/components/start_icon.php
@@ -1,4 +1,4 @@
-<svg viewBox="0 0 100 100" width="30px" height="30px" stroke="currentColor" style="stroke-width: 10px;">
+<svg viewBox="0 0 100 100" width="25px" height="25px" stroke="currentColor" style="stroke-width: 10px;">
     <polygon points="10,85 90,85 50,15" style="fill:none;transform-origin: 50px 50px;"
         transform="matrix(-1, 0, 0, -1, 0, 0)" />
 </svg>
