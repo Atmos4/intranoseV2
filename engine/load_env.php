10049fd4062ef15112f063e217b915a77b5bd60f
Atmos4 <atmos4.dev@gmail.com>
2024-11-21 19:25:27 +0100

    326: update README

--- engine/load_env.php
+++ engine/load_env.php
@@ -7,13 +7,6 @@ if (!file_exists(base_path() . "/.env")) {
 $dotenv = Dotenv\Dotenv::createImmutable(base_path());
 $dotenv->load();
 
-// Database
-if (env("DB_HOST")) {
-    $dotenv->required('DB_NAME');
-    $dotenv->required('DB_USER');
-    $dotenv->required('DB_PASSWORD');
-}
-
 // Mailing - unused at the moment
 // AP 2024-07 - TODO FIXME mailing does not work, we need to plug it into Gmail
 $dotenv->ifPresent('USE_DKIM')->isBoolean();
