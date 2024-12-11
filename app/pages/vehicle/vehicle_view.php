3b3e671286c179627881effd54c7e95d5285cd65
Mathieu Perrin <46241768+Mathieu414@users.noreply.github.com>
2024-12-11 20:20:38 +0100

    Car management 🚗 (#318)
    * 276: Add vehicle functionality 🚗
    
    * 276: Car management 2.0
    
    * 276: add dates to vehicles
    
    * 276: add feature handling
    
    * 276: remove unused migration flie
    
    ---------
    
    Co-authored-by: Atmos4 <atmos4.dev@gmail.com>

---
+++ app/pages/vehicle/vehicle_view.php
@@ -0,0 +1,20 @@
+<?php
+restrict_access();
+$user_id = User::getCurrent()->id;
+$event = EventService::getEventWithAllData(get_route_param('event_id'), $user_id);
+
+$vehicles = em()->createQuery('SELECT v FROM Vehicle v WHERE v.event = ?1')->setParameter(
+    1,
+    $event->id
+)->getResult();
+
+?>
+
+<?php foreach ($vehicles as $vehicle): ?>
+    <div hx-get="/evenements/<?= $event->id ?>/vehicule/<?= $vehicle->id ?>/inscription/<?= $user_id ?>" hx-trigger="load"
+        hx-target="this">
+    </div>
+<?php endforeach ?>
+
+<a role=button class="secondary" href="/evenements/<?= $event->id ?>/vehicule/nouveau">
+    <i class="fas fa-plus"></i> Ajouter un véhicule</a>
