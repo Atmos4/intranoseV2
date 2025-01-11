454852dffcdd408621bebf7b993656961f41c8b7
Mathieu Perrin <mathieu.perrin@insa-lyon.fr>
2025-01-11 08:19:58 +0100

    357: Add color themes to clubs

--- database/models/clubs.db.php
+++ database/models/clubs.db.php
@@ -13,10 +13,13 @@ class Club
     #[Column]
     public string $name; // Intranose
 
+    #[Column(options: ["default" => "green"])]
+    public ThemeColor $themeColor;
     function __construct($name, $slug)
     {
         $this->name = $name;
         $this->slug = $slug;
+        $this->themeColor = ThemeColor::green;
     }
 
     function toForm()
@@ -24,6 +27,57 @@ class Club
         return [
             "name" => $this->name,
             "slug" => $this->slug,
+            "color" => $this->themeColor->value
         ];
+    }
+}
+
+enum ThemeColor: string
+{
+    case fuchsia = "fuchsia";
+    case green = "green";
+    case grey = "grey";
+    case indigo = "indigo";
+    case jade = "jade";
+    case lime = "lime";
+    case orange = "orange";
+    case pink = "pink";
+    case pumpkin = "pumpkin";
+    case purple = "purple";
+    case red = "red";
+    case sand = "sand";
+    case slate = "slate";
+    case violet = "violet";
+    case yellow = "yellow";
+    case zinc = "zinc";
+
+    public function getColorCode(): string
+    {
+        return match ($this) {
+            self::fuchsia => '#c1208b',
+            self::green => '#398712',
+            self::grey => '#ababab',
+            self::indigo => '#524ed2',
+            self::jade => '#007a50',
+            self::lime => '#a5d601',
+            self::orange => '#d24317',
+            self::pink => '#d92662',
+            self::pumpkin => '#ff9500',
+            self::purple => '#9236a4',
+            self::red => '#c52f21',
+            self::sand => '#ccc6b4',
+            self::slate => '#525f7a',
+            self::violet => '#7540bf',
+            self::yellow => '#f2df0d',
+            self::zinc => '#646b79',
+        };
+    }
+
+    static function colorsList(): array
+    {
+        return array_reduce(ThemeColor::cases(), function ($carry, $c) {
+            $carry[$c->value] = $c->getColorCode();
+            return $carry;
+        }, []);
     }
 }
