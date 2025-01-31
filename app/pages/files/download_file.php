610cde25ed9163d0213bf6021282e1a11e4f0db0
Mathieu Perrin <mathieu.perrin@insa-lyon.fr>
2025-01-31 10:44:33 +0100

    377: fix permission level

--- app/pages/files/download_file.php
+++ app/pages/files/download_file.php
@@ -4,6 +4,9 @@ $file_id = $_GET['id'];
 
 $db = DB::getInstance();
 $file = $db->em()->find(SharedFile::class, $file_id);
+if (in_array($file->permission_level, Access::$ADD_EVENTS)) {
+    restrict_access(Access::$ADD_EVENTS);
+}
 $path = Path::uploads($file->path);
 
 if (file_exists($path)) {
