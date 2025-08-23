ceb3e0594bae96404340e4f13ab90487e195506e
Mathieu Perrin <mathieu.perrin@insa-lyon.fr>
2025-08-23 23:15:34 +0200

    407: improve calendar

--- app/pages/events/event_list/event_cal_day.php
+++ app/pages/events/event_list/event_cal_day.php
@@ -1,10 +1,11 @@
 <?php
 include_once __DIR__ . "/RenderEvents.php";
 $date = $_GET['date'] ?? date('Y-m-d');
+$formatted_date = new DateTime($date);
 $events = EventService::getEventsForDay($date, User::getCurrentUserId());
 
 if (!$events) { ?>
-    <p style="text-align:center">Rien aujourd'hui</p>
+    <p style="text-align:center">Rien le <?= $formatted_date->format("d/m") ?> </p>
 <?php }
 
 foreach ($events as $e) {
