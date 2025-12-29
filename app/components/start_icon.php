0bdd7bc6c09ec39a1e5893f9d796a960f892bd43
Mathieu Perrin <mathieu.perrin@insa-lyon.fr>
2025-12-29 12:01:58 +0100

    462: add end date to activities

--- app/components/start_icon.php
+++ app/components/start_icon.php
@@ -1,4 +1,3 @@
 <svg viewBox="0 0 100 100" width="25px" height="25px" stroke="currentColor" style="stroke-width: 10px;">
-    <polygon points="10,85 90,85 50,15" style="fill:none;transform-origin: 50px 50px;"
-        transform="matrix(-1, 0, 0, -1, 0, 0)" />
+    <polygon points="10,85 90,85 50,15" style="fill:none;transform-origin: 50px 50px;" transform="rotate(90)" />
 </svg>
